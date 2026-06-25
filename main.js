1b44891e9aef6580e15562b566001b3bce9a724a
kamyla-vandoski <vandoski.kamyla@escola.pr.gov.br>
2026-06-25 12:56:56 -0300

    e76j

--- main.js
+++ main.js
@@ -4,19 +4,18 @@ numeroSenha.textContent = tamanhoSenha;
 
 const botoes = document.querySelectorAll('.parametro-senha__botao');
 
-botoes[0].onclik = diminuiTamanho;
-botoes[1].onclik = aumentaTamanho;
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
 
-function diminuiTamanho(){
-    if (tamanhoSenha >1){
-        //tamanhoSenha = tamanhoSenha-1;
+function diminuiTamanho() {
+    if (tamanhoSenha > 1) {
         tamanhoSenha--;
     }
-numeroSenha.textContent = tamanhoSenha;
+    numeroSenha.textContent = tamanhoSenha;
 }
-function aumentaTamanho(){
-    if(tamanhoSenha <20){
-        //tamanhoSenha = tamanhoSenha+1;
+
+function aumentaTamanho() {
+    if (tamanhoSenha < 20) {
         tamanhoSenha++;
     }
     numeroSenha.textContent = tamanhoSenha;
