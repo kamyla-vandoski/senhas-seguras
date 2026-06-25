f30d78972857f869d5c9024e10354fca71bb641e
kamyla-vandoski <vandoski.kamyla@escola.pr.gov.br>
2026-06-25 08:29:25 -0300

    earv

--- main.js
+++ main.js
@@ -4,4 +4,9 @@ numeroSenha.textContent = 5;
 
 const botoes = document.querySelectorAll('.parametro-senha__botao');
 
+botoes[0].onclik = diminuiTamanho;
+
+function diminuiTamanho(){
+    
+}
 console.log(botoes);
