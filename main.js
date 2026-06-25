7454ecf4092e2cd8f2f1712ffbd76a415c190443
kamyla-vandoski <vandoski.kamyla@escola.pr.gov.br>
2026-06-25 08:25:46 -0300

    idt6kju67

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 const numeroSenha = document.querySelector('.parametro-senha__texto');
 
 numeroSenha.textContent = 5;
+
+const botoes = document.querySelector('.parametro-senha__botao');
+
+console.log(botoes);
