bb3738ab4bd8cfc2319ebab0b030cba20b7af960
kamyla-vandoski <vandoski.kamyla@escola.pr.gov.br>
2026-07-09 13:20:42 -0300

    nsgf

--- main.js
+++ main.js
@@ -29,6 +29,7 @@ geraSenha();
 
 function geraSenha(){
 let numeroAleatorio = Math.random()*letrasMaiusculas.length;
+numeroAleatorio=Math.floor(numeroAleatorio);
 console.log(numeroAleatorio);
 }
 
